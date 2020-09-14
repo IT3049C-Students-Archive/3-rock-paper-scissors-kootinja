6a22f63143bc7fc87de3b9864aa2fdcb3eae7295
Jeanette <jkootinsanwu@gmail.com>
2020-09-13 22:40:12 -0400

    finished game logic

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -14,8 +14,8 @@ class RockPaperScissors {
    */
   generateCPUResponse(){
     const acceptedValues = [ `rock`, `paper`, `scissors` ];
-
-    return ;
+    Math.random(0, 1, 2); 
+      return acceptedValues;
   }
   /**
    * returns one of the following values: `win`, `lose`, `tie`
@@ -33,6 +33,37 @@ class RockPaperScissors {
    * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   determineWinner(userSelection, cpuSelection){
+    //Having a tie
+    if (userSelection === cpuSelection){
+      return "It's a tie!";
+    }
+    //If the user types in scissors
+  else if(userSelection === "scissors"){
+    if(cpuSelection === "rock"){
+      return "Computer wins! Rock kills scissors.";
+    }
+    else if(cpuSelection ==="paper"){
+      return "User wins! Scissors kill paper.";
+    }
+  }
+    //If the user types in paper
+  else if(userSelection === "paper"){
+    if(cpuSelection === "rock"){
+      return "User wins! Paper kills rock.";
+    }
+    else if(cpuSelection === "scissors"){
+      return "Computer wins! Scissors kill paper.";
+    }
+  }
+    //If the user types in rock
+  else if(userSelection === "rock"){
+    if(cpuSelection === "paper"){
+      return "Computer wins! Paper kills rock.";
+    }
+    else if(cpuSelection === "scissors"){
+      return "User wins! Rock kills scissors."
+    };
+  }
 
   }
 
@@ -41,6 +72,11 @@ class RockPaperScissors {
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   play(userSelection){
+    // if the user won the round
+    this.score.user ++;
+    // if the user cpu the round
+    this.score.cpu ++;
+    this.gameHistoryLog.push(`Jeanette selected Scissors, CPU selected Paper: Jeanette wins wins`);
 
   }
 
